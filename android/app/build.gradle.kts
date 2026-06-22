4af336c0ee659e42b23c63c9d3b87eaede458d5c
Fatima <fatimatulzahra7802@gmail.com>
2026-06-22 13:34:04 +0500

    Revert "first commit"
    This reverts commit 6e1568710406759b2b0be45e5b1db934d7718f32.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.eagalesoft.blood_donation_app"
+        applicationId = "com.example.blood_donation_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
